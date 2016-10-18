058823f9a770804e27e9199866b93d8777de6491
Obadha <wobadha@gmail.com>
2016-10-18 20:44:57 +0300

    Added contacts file

---
+++ contacts.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Contacts(db.Model):
+
+	__tablename__ = 'members'
+
+	phoneNumber = db.Column(db.String(20),primary_key=True) #this is captured automatically
+	name = db.Column(db.String(50), nullable = False)
+	age = db.Column(db.String(10), nullable = False)
+	gender = db.Column(db.String(20), nullable = False)
+
+	def __repr__ (self):
+		#from this we will get the name to address them with in the call.
+		return '<name %r>' % self.name
