acf1b111ab418bd542dde08fec329ec987d3ee74
Obadha <wobadha@gmail.com>
2017-01-17 20:50:09 +0300

    included welcome message(response), yet to add url to audio recording.

--- app/api/voice.py
+++ app/api/voice.py
@@ -17,34 +17,35 @@ def voice_callback():
 	callerNumber = request.values.get('callerNumber', None)
 
 	if isActive == '1':
-		callerNumber = request.values.get('callerNumber', None)
-		logging.info('caller is {}'.format(callerNumber))
+		callerNumber = request.values.get('callerNumber', None) #gets the number of caller
+		logging.info("caller is {}".format(callerNumber)) #logs number of caller
 
-		response = '<Response>'
-		response += '<Say maxDuration="60" playBeep="false"> Hi, '
-		response += 'Welcome Mr </Say>'
+		response = '<Response>' #Beginning of audio response that will be played out.
+		response += '<Say maxDuration="60" playBeep="false"> Welcome, Mr </Say>'
 		response += 'name="{}"'.format(name)
-		response += '<Say> For inspiration messages on overcoming procrastination, Press 1'
-		response += 'For messages on self-improvement, Press 2 </Say>'
+		response += '<Play url ="link-to-audio-file">' #this will be picking a random audio file
 
-		'''
-		User has just been prompted to input key.
-		Here will come in code to capture that input.
-		Below is code that will act based on input.
-		'''
-		if input == 1:
-			#i suppose the best way would be, have all the audio files
-			#online somewhere, then here call them randomly based on selection(input) made.
+		
+		
+		response += "</Response>"
 
-			'''
-			if selection is 1:
-				play random audio file in that category
-			elif selection is 2:
-				play random audio file in that category
-			elif selection is 3:
-				play random audio file in that category
-			'''
-			
+		voice = Voice.(sessionId=sessionId,url='')
+		db.session.add(voice)
+		db.session.commit()
+
+	else:
+		voice = Voice.query.filter_by(sessionId = sessionId).first()
+		voice.callCost = request.values.get('amount', None)
+
+		db.session.add(voice)
+		db.session.commit()
+		logging.info(request.values)
+
+		response = ""
+
+	resp = make_response(response, 200)
+	resp.headers['Content-Type'] = "application/xml"
+	return resp
 
 
 ###to be continued
